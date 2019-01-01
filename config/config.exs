0ab116614046385f668dc7366bfc43152ad72868
t-kusakabe <ttmac0704@gmail.com>
2019-01-01 20:11:39 +0900

    Update config/config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,9 @@
 use Mix.Config
 
+config :example_app, ecto_repos: [ExampleApp.Repo]
+
 config :example_app, ExampleApp.Repo,
-  database: "example_app_repo",
-  username: "user",
-  password: "pass",
+  database: "example_app",
+  username: "postgres",
+  password: "",
   hostname: "localhost"
